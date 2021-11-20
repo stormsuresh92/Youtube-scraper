209cc3883fd7bf3602d30e7677e64c949146ecea
stormsuresh92 <38948398+stormsuresh92@users.noreply.github.com>
2021-11-20 15:37:10 +0530

    Update Youtube_Overall.py

--- Youtube_Overall.py
+++ Youtube_Overall.py
@@ -1,51 +1,67 @@
 from requests_html import HTMLSession
-import os
 import pandas as pd
+from alive_progress import alive_bar
+from time import sleep
 
 
 s = HTMLSession()
 
+headers = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36',
+    'Connection':'keep-alive'
+}
+
 search = input('Enter keyword here: ')
-print('printing data.....')
+print('Printing data.....')
 
 data = []
 
 url = f'https://www.youtube.com/results?search_query={search}&sp=CAISAhAB'
-response = s.get(url)
-response.html.render(sleep=2, timeout=500, keep_page=True, scrolldown=100)
+response = s.get(url, headers=headers)
+response.html.render(sleep=5, timeout=100, keep_page=True, scrolldown=3)
 contents = response.html.find('#dismissible')
-for item in contents:
-    try:
-        Channel = item.find('#text > a', first=True).text
-    except:
-        Channel = ''
-    try:
-        Title = item.find('#video-title', first=True).text
-    except:
-        Title = ''
-    try:
-        Views = item.find('#metadata-line > span:nth-child(1)', first=True).text.replace('views', '')
-    except:
-        Views = ''
-    try:    
-        Posted = item.find('#metadata-line > span:nth-child(2)', first=True).text
-    except:
-        Posted = ''
-    try:
-        url = 'https://www.youtube.com' + item.find('#video-title', first=True).attrs['href']
-    except:
-        url = ''
-        
-    dic = {
-        'Channel':Channel,
-        'Video_Title':Title,
-        'Views':Views,
-        'Posted':Posted,
-        'Url':url
-        }
-    data.append(dic)
+with alive_bar(len(contents), bar='classic2', spinner='classic') as bar:
+    for item in contents:
+        try:
+            Channel = item.find('#text > a', first=True).text
+        except:
+            Channel = ''
+        try:
+            Title = item.find('#video-title', first=True).text
+        except:
+            Title = ''
+        try:
+            Views = item.find('#metadata-line > span:nth-child(1)', first=True).text.replace('views', '')
+        except:
+            Views = ''
+        try:    
+            Posted = item.find('#metadata-line > span:nth-child(2)', first=True).text
+        except:
+            Posted = ''
+        try:
+            Description = item.find('.metadata-snippet-container.style-scope.ytd-video-renderer', first=True).text
+        except:
+            Description = ''
+        try:
+            url = 'https://www.youtube.com' + item.find('#video-title', first=True).attrs['href']
+        except:
+            url = ''
+            
+        dic = {
+            'Channel':Channel,
+            'Video_Title':Title,
+            'Views':Views,
+            'Posted':Posted,
+            'Description':Description,
+            'Url':url
+            }
+        data.append(dic)
+        sleep(1)
+        bar()
 
     
 df = pd.DataFrame(data)
 df.to_csv(f'{search}.csv', index=False)
-print('download finished')
+print('\n')
+print('Download completed')
+input()
